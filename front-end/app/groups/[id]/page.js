5bef03523af58da4f3ee58919ee00a0d950ea6f4
blakeminix <blakeminix2@gmail.com>
2024-05-07 10:22:27 -0400

    Updated Folder System

--- front-end/app/groups/[id]/page.js
+++ front-end/app/groups/[id]/page.js
@@ -1,17 +1,16 @@
 import Link from 'next/link'
 
 export const metadata = {
-  title: "Dashboard | CMS",
+  title: "Group | CMS",
   description: "A content management system developed using React/Next.js for the front-end, Express.js/Node.js for the back-end, and MySQL for the back-end database.",
 };
  
 export default function Page() {
   return (
     <div>
-    <Link href="/login">Login</Link>
+    <Link href="/.">Dashboard</Link>
     <Link href="/settings">Settings</Link>
     <Link href="/[username]" as="/blakeminix">Profile</Link>
-    <Link href="/groups/[id]" as="/groups/1">Group</Link>
     </div>
   );
 }
