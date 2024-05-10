8271ab75dcef84861913cbd9a43ecbcfbf2adf09
blakeminix <blakeminix2@gmail.com>
2024-05-10 13:07:16 -0400

    Init Login Test

---
+++ front-end/app/(homepage)/page.js
@@ -0,0 +1,22 @@
+import { redirect } from 'next/navigation'
+import Link from 'next/link'
+import "../globals.css"
+
+export const metadata = {
+  title: "Dashboard | CMS",
+  description: "A content management system developed using React/Next.js for the front-end, Express.js/Node.js for the back-end, and MySQL for the back-end database.",
+};
+ 
+export default function Page() {
+  //redirect('/login')
+  return (
+    <div>
+      <div className="group-row">
+        <Link href="/login">Login</Link>
+        <Link href="/groups/[id]" as="/groups/1">Group 1</Link>
+        <Link href="/groups/[id]" as="/groups/2">Group 2</Link>
+        <Link href="/groups/[id]" as="/groups/3">Group 3</Link>
+      </div>
+    </div>
+  );
+}
---
+++ front-end/app/components/Login.js
@@ -0,0 +1,48 @@
+'use client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation'
+import React from 'react';
+
+export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const router = useRouter()
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+      // Perform authentication logic here (e.g., send credentials to backend)
+      // Replace the following logic with your actual authentication logic
+      if (username === 'admin' && password === 'password') {
+        // Redirect to dashboard if authentication succeeds
+        router.push('/');
+      } else {
+        // Show error message or handle unsuccessful login
+        console.error('Invalid credentials');
+      }
+    };
+  
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Username</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
