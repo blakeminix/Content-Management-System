2b63ec34fbeb99f74a6e915fe264253d4127f820
blakeminix <blakeminix2@gmail.com>
2024-05-24 20:15:33 -0400

    Add description to group

---
+++ app/api/adddescription/route.js
@@ -0,0 +1,17 @@
+import { addDescription } from "@/app/lib";
+import { NextResponse } from "next/server";
+
+export async function POST(req, res) {
+  try {
+    const message = await req.json();
+    const {
+      description,
+      gid
+    } = message;
+    await addDescription(description, gid);
+    return NextResponse.json({ message: 'Description stored successfully' });
+  } catch (error) {
+    console.error('Logout failed:', error);
+    return NextResponse.json({ message: 'Description store failed' });
+  }
+}
