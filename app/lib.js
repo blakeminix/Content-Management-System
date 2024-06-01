ab0a399a2a4d13a0ffbca21cc23d886cdd87cd0e
blakeminix <blakeminix2@gmail.com>
2024-06-01 10:17:43 -0400

    Port

--- app/lib.js
+++ app/lib.js
@@ -13,7 +13,7 @@ const key = new TextEncoder().encode(secretKey);
 
 const pool = mysql.createPool({
   host: process.env.HOST,
-  port: '3307',
+  port: '3306',
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
