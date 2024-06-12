4938e7ba3bc6157156f71da3694d0f2a431ae553
blakeminix <blakeminix2@gmail.com>
2024-06-12 16:53:19 -0400

    Profile loading

--- app/creategroup/page.js
+++ app/creategroup/page.js
@@ -29,13 +29,15 @@ export default async function Page() {
     redirect('/.');
   }
   return (
-    <div>
-      <div className="top-bar">
-        <Link href="/dashboard">Dashboard</Link>
-        <Link href="/creategroup">Create Group</Link>
-        <ProfileDropdown />
+    <div className='min-h-screen'>
+      <div className="flex items-center h-16 fixed top-0 w-full z-50 p-3 bg-gray-800 text-white shadow-md">
+        <div className="flex items-center flex-grow">
+          <Link className="px-4 py-2 text-lg font-semibold hover:bg-gray-700 rounded transition-colors duration-300" href="/dashboard">Dashboard</Link>
+          <Link className="px-4 py-2 text-lg font-semibold hover:bg-gray-700 rounded transition-colors duration-300" href="/creategroup">Create Group</Link>
+          <ProfileDropdown />
+        </div>
       </div>
-    <section className='centered'>
+      <section className='centered'>
       <div>
       <form
         action={async (formData) => {
@@ -44,7 +46,7 @@ export default async function Page() {
           redirect(`/groups/${id}`);
         }}
       >
-        <input className="text-box" type="username" name="group_name" placeholder="Group Name" maxLength={24}/>
+        <input className="text-box" type="username" name="group_name" placeholder="Group Name" maxLength={24} autoComplete='off'/>
         <br />
         <br />
         <div>
@@ -60,7 +62,7 @@ export default async function Page() {
         <button className="login-button" type="submit">Create Group</button>
       </form>
       </div>
-    </section>
+      </section>
     </div>
   );
 }
