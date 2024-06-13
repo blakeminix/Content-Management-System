bbf63bc4e7a37684ebb9ba8871d077b0ac892221
blakeminix <blakeminix2@gmail.com>
2024-06-13 14:58:14 -0400

    Group users page

--- app/(homepage)/groups/[id]/users/page.js
+++ app/(homepage)/groups/[id]/users/page.js
@@ -36,13 +36,14 @@ export default async function Page() {
   }
 
   return (
-    <div>
-      <div className="top-bar">
-        <Link href="/dashboard">Dashboard</Link>
-        <Link href="/creategroup">Create Group</Link>
-        <ProfileDropdown />
+    <div className='min-h-screen'>
+      <div className="flex items-center h-16 fixed top-0 w-full z-50 p-3 bg-gray-800 text-white shadow-md">
+        <div className="flex items-center flex-grow">
+          <Link className="px-4 py-2 text-lg font-semibold hover:bg-gray-700 rounded transition-colors duration-300" href="/dashboard">Dashboard</Link>
+          <Link className="px-4 py-2 text-lg font-semibold hover:bg-gray-700 rounded transition-colors duration-300" href="/creategroup">Create Group</Link>
+          <ProfileDropdown />
+        </div>
       </div>
-      <SideBar />
       <Users />
     </div>
   );
